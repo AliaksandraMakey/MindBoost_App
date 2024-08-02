beccd0310ecc2d112bec8c3c8e8fc67c2e64f7ab
Homan Alexandra <goman.a@softintermob.com>
2024-08-02 20:01:14 +0400

    Initial Commit

---
+++ MindBoost_App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MindBoost_App
+//
+//  Created by Alexandra Makey on 02.08.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MindBoost_App/MindBoost_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindBoost_AppApp.swift
+//  MindBoost_App
+//
+//  Created by Alexandra Makey on 02.08.24.
+//
+
+import SwiftUI
+
+@main
+struct MindBoost_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
